900379db2f08fc7e3a5cd53da3ce43b58cb2bb48
darce <daniel.arce@gmail.com>
2023-05-19 20:43:07 -0400

    init map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React, { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <header>
-      </header>
-    </div>
-  );
-}
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 
-export default App;
+const App = () => {
+    const mapContainerRef = useRef(null)
+
+    /** Initialize map when component mounts */
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            /** Avaiable style options: https://docs.mapbox.com/api/maps/#styles */
+            style: 'mapbox://styles/mapbox/navigation-night-v1',
+            center: [-73.9442345, 40.7191821],
+            zoom: 15.5
+        })
+
+        /** Add map controls */
+        map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+
+        /** Clean up on unmount */
+        return () => map.remove()
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+        return (
+            <div>
+                <div className="map-container" ref={mapContainerRef} />
+            </div>
+        );
+    }
+
+export default App
