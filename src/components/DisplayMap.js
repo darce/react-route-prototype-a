137a7b53e28cbc31216e8b71075d379f19ccf3fe
darce <daniel.arce@gmail.com>
2023-05-23 17:07:14 -0400

    add on move event listener

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -1,24 +1,47 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
 
 const DisplayMap = () => {
     const mapContainerRef = useRef(null)
+    const mapRef = useRef(null)
+    const [lng, setLng] = useState([-73.9442345])
+    const [lat, setLat] = useState([40.7191821])
+    const [zoom, setZoom] = useState([13])
+
     /** Initialize map when component mounts */
     useEffect(() => {
-        const map = new mapboxgl.Map({
+        if (mapRef.current) return // initialize map only once
+        mapRef.current = new mapboxgl.Map({
             container: mapContainerRef.current,
+            center: [lng, lat],
+            zoom: zoom,
             /** Avaiable style options: https://docs.mapbox.com/api/maps/#styles */
-            style: 'mapbox://styles/mapbox/navigation-night-v1',
-            center: [-73.9442345, 40.7191821],
-            zoom: 13
+            style: 'mapbox://styles/mapbox/navigation-night-v1'
         })
-    /** Add map controls */
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
-    }, [])
+    })
 
-    return(
+    /** On map move */
+    useEffect(() => {
+        if(!mapRef.current) return // wait for initalization
+        mapRef.current.on('move', () => {
+            setLng(mapRef.current.getCenter().lng.toFixed(4))
+            setLat(mapRef.current.getCenter().lat.toFixed(4))
+            setZoom(mapRef.current.getZoom().toFixed(2))
+        })
+
+    })
+
+
+    return (
+        <div>
+            <div className ="coords-container">
+                Longitude: {lng} | Latitude: {lat} | Zoom {zoom}
+            </div>
         <div className="map-container" ref={mapContainerRef} />
+        </div>
     )
 }
 
