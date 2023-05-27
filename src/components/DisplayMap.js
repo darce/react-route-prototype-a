dfb07817a6517bcf950d69999d0405d43867f9b3
darce <daniel.arce@gmail.com>
2023-05-27 12:44:14 -0400

    implement dynamic geojson on click handler

--- src/components/DisplayMap.js
+++ src/components/DisplayMap.js
@@ -10,6 +10,7 @@ const DisplayMap = () => {
     const [lng, setLng] = useState([-73.9442345])
     const [lat, setLat] = useState([40.7191821])
     const [zoom, setZoom] = useState([13])
+    const [routeCoords, setRouteCoords] = useState([])
 
     /** Initialize map when component mounts */
     useEffect(() => {
@@ -21,26 +22,80 @@ const DisplayMap = () => {
             /** Avaiable style options: https://docs.mapbox.com/api/maps/#styles */
             style: 'mapbox://styles/mapbox/navigation-night-v1'
         })
-    })
+
+        mapRef.current.on('load', () => {
+            if (!mapRef.current.getSource('route-source')) {
+                mapRef.current.addSource('route-source', {
+                    'type': 'geojson',
+                    data: {
+                        'type': 'Feature'
+                    }
+                })
+            }
+            if (mapRef.current.getLayer('route-layer')) return
+            mapRef.current.addLayer({
+                'id': 'route-layer',
+                'type': 'line',
+                'source': 'route-source',
+                'layout': {
+                    'line-join': 'round',
+                    'line-cap': 'round'
+                },
+                'paint': {
+                    'line-color': '#c0ff00',
+                    'line-width': 4
+                }
+            })
+        })
+    }, [])
 
     /** On map move */
     useEffect(() => {
-        if(!mapRef.current) return // wait for initalization
+        if (!mapRef.current) return // wait for initalization
         mapRef.current.on('move', () => {
             setLng(mapRef.current.getCenter().lng.toFixed(4))
             setLat(mapRef.current.getCenter().lat.toFixed(4))
             setZoom(mapRef.current.getZoom().toFixed(2))
         })
-
     })
 
+    /** On map click */
+    useEffect(() => {
+        if (!mapRef.current) return // wait for initalization
+        mapRef.current.on('click', handleClickPath)
+        return () => {
+            mapRef.current.off('click', handleClickPath);
+        };
+    }, [routeCoords])
+
+    const handleClickPath = (e) => {
+        const clickedCoords = e.lngLat;
+        setRouteCoords(routeCoords.concat(clickedCoords))
+        console.log('coords', routeCoords)
+        const geojsonSource = mapRef.current.getSource('route-source')
+        geojsonSource.setData({
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'LineString',
+                coordinates: spreadCoords([...routeCoords, clickedCoords]),
+
+            }
+        })
+    }
+
+    const spreadCoords = (coordsArray) => {
+        const spreadCoords = coordsArray.map(coords => [coords.lng, coords.lat])
+        console.log(spreadCoords)
+        return spreadCoords
+    }
 
     return (
         <div>
-            <div className ="coords-container">
+            <div className="coords-container">
                 Longitude: {lng} | Latitude: {lat} | Zoom {zoom}
             </div>
-        <div className="map-container" ref={mapContainerRef} />
+            <div className="map-container" ref={mapContainerRef} />
         </div>
     )
 }
